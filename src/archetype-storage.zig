30993ba58a1bcf16f97f4a9c2e04f9996db9e562
Dorian <dorian@resalocal.com>
2023-05-01 15:16:39 +0200

    prepare arch storage

---
+++ src/archetype-storage.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const Component = @import("./component.zig").Component;
+const ArchetypeMask = @import("./archetype.zig").ArchetypeMask;
+const Archetype = @import("./archetype.zig").Archetype;
+const ArchetypeEdge = @import("./archetype.zig").ArchetypeEdge;
+
+// pub const ArchetypeStorage = struct {
+//     const Self = @This();
+//     allocator: std.mem.Allocator,
+//     archetypes:
+
+//     pub fn init() ArchetypeStorage {}
+// };
