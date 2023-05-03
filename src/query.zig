23684fe9c96a4f656c24665a2359d28b1b4d1418
Dorian <dorian@resalocal.com>
2023-05-03 22:38:46 +0200

    clean

--- src/query.zig
+++ src/query.zig
@@ -4,63 +4,9 @@ const Archetype = @import("./archetype.zig").Archetype;
 const World = @import("./world.zig").World;
 const Entity = @import("./entity-storage.zig").Entity;
 
-fn numMasks(bit_length: usize) usize {
-    return (bit_length + (@bitSizeOf(std.bit_set.DynamicBitSet.MaskInt) - 1)) / @bitSizeOf(std.bit_set.DynamicBitSet.MaskInt);
-}
-fn contains(bitset: *const std.bit_set.DynamicBitSet, other: *std.bit_set.DynamicBitSet) bool {
-    const len = @min(numMasks(bitset.unmanaged.bit_length), numMasks(other.unmanaged.bit_length));
+pub const MAX_COMPONENTS_PER_QUERY_MATCHER = 100;
 
-    for (bitset.unmanaged.masks[0..len]) |*mask, i| {
-        if (mask.* & other.unmanaged.masks[i] != mask.*) {
-            return false;
-        }
-    }
-
-    return true;
-}
-fn intersects(bitset: *const std.bit_set.DynamicBitSet, other: *std.bit_set.DynamicBitSet) bool {
-    const len = @min(numMasks(bitset.unmanaged.bit_length), numMasks(other.unmanaged.bit_length));
-
-    for (bitset.unmanaged.masks[0..len]) |*mask, i| {
-        if (mask.* & other.unmanaged.masks[i] > 0) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-pub const QueryIterator = struct {
-    const Self = @This();
-    archetypes: *std.ArrayList(*Archetype),
-
-    current_archetype_index: usize = 0,
-    current_entity_index: usize = 0,
-
-    pub fn next(self: *Self) ?Entity {
-        if (self.current_archetype_index < self.archetypes.items.len) {
-            var archetype_entities = self.archetypes.items[self.current_archetype_index].entities;
-            if (self.current_entity_index < archetype_entities.count) {
-                self.current_entity_index += 1;
-                return archetype_entities.values[self.current_entity_index - 1]; // entities start at 1
-            } else {
-                self.current_entity_index = 0;
-                self.current_archetype_index += 1;
-                return self.next();
-            }
-        } else {
-            return null;
-        }
-    }
-
-    pub fn count(self: *Self) u64 {
-        var len: u64 = 0;
-        for (self.archetypes.items) |arch| {
-            len += arch.entities.count;
-        }
-        return len;
-    }
-};
+pub const QueryMatcherType = enum { any, all, not, none };
 
 pub const Query = struct {
     const Self = @This();
@@ -72,24 +18,12 @@ pub const Query = struct {
     allocator: std.mem.Allocator,
 
     pub fn init(matchers: std.ArrayList(QueryMatcher), allocator: std.mem.Allocator) Query {
-        // var operations_buffer: [10]QueryMatcher = undefined;
-        // _ = operations_buffer;
-        var query = Query{
+        return Query{
             .allocator = allocator,
             .matchers = matchers,
             .archetypes = std.ArrayList(*Archetype).init(allocator),
         };
-
-        return query;
     }
-
-    // pub fn each(self: *Self, function: fn (entity: Entity) void) void {
-    //     for (self.archetypes) |arch| {
-    //         for (arch.values) |entity| {
-    //             function(entity);
-    //         }
-    //     }
-    // }
 
     pub fn iterator(self: *Self) QueryIterator {
         return QueryIterator{
@@ -102,6 +36,7 @@ pub const Query = struct {
         for (self.matchers.items) |*matcher| {
             matcher.deinit();
         }
+        self.matchers.deinit();
     }
 
     fn execute(self: *Self, world: *World) void {
@@ -125,10 +60,6 @@ pub const Query = struct {
     }
 };
 
-pub const MAX_COMPONENTS_PER_QUERY_MATCHER = 100;
-
-pub const QueryMatcherType = enum { any, all, not, none };
-
 pub const QueryMatcher = struct {
     const Self = @This();
     op_type: QueryMatcherType,
@@ -147,36 +78,6 @@ pub const QueryMatcher = struct {
     }
 };
 
-// pub fn QueryOperation(config: type) type {
-//     _ = config;
-//     return struct {
-//         const Self = @This();
-//     };
-// }
-
-// pub const QueryOperationTag = enum { any, all };
-
-// pub const QueryOperation = union(QueryOperationTag) {
-//     const Self = @This();
-//     any: std.bit_set.DynamicBitSet,
-//     all: std.bit_set.DynamicBitSet,
-
-//     pub fn match(self: Self, archetype: *Archetype) bool {
-//         return switch (self) {
-//             .any => |*mask| intersects(mask, &archetype.mask),
-//             .all => |*mask| contains(mask, &archetype.mask),
-//         };
-//     }
-
-//     pub fn clone(self: Self, allocator: std.mem.Allocator) Self {
-//         return switch (self) {
-//             .any => |*mask| QueryOperation{ .any = mask.clone(allocator) catch unreachable },
-//             .all => |*mask| QueryOperation{ .all = mask.clone(allocator) catch unreachable },
-//         };
-//     }
-// };
-
-const QUERY_MATCHERS_LIST_CAPACITY = 20;
 pub const QueryBuilder = struct {
     const Self = @This();
 
@@ -185,9 +86,10 @@ pub const QueryBuilder = struct {
     matchers: std.ArrayList(QueryMatcher),
 
     pub fn init(allocator: std.mem.Allocator) !QueryBuilder {
-        var matchers = std.ArrayList(QueryMatcher).init(allocator);
-        matchers.ensureTotalCapacity(QUERY_MATCHERS_LIST_CAPACITY) catch unreachable;
-        return QueryBuilder{ .allocator = allocator, .matchers = matchers };
+        return QueryBuilder{
+            .allocator = allocator,
+            .matchers = std.ArrayList(QueryMatcher).init(allocator),
+        };
     }
 
     pub fn deinit(self: *Self) void {
@@ -228,7 +130,7 @@ pub const QueryBuilder = struct {
             mask.set(component.id);
         }
 
-        self.matchers.appendAssumeCapacity(QueryMatcher{ .op_type = matcher_type, .mask = mask });
+        self.matchers.append(QueryMatcher{ .op_type = matcher_type, .mask = mask }) catch unreachable;
     }
 
     pub fn from(self: *Self, world: *World) Query {
@@ -236,10 +138,68 @@ pub const QueryBuilder = struct {
 
         self.matchers.clearAndFree();
 
-        self.matchers.ensureTotalCapacity(QUERY_MATCHERS_LIST_CAPACITY) catch unreachable;
-
         created_query.execute(world);
 
         return created_query;
     }
 };
+
+pub const QueryIterator = struct {
+    const Self = @This();
+    archetypes: *std.ArrayList(*Archetype),
+
+    current_archetype_index: usize = 0,
+    current_entity_index: usize = 0,
+
+    pub fn next(self: *Self) ?Entity {
+        if (self.current_archetype_index < self.archetypes.items.len) {
+            var archetype_entities = self.archetypes.items[self.current_archetype_index].entities;
+            if (self.current_entity_index < archetype_entities.count) {
+                self.current_entity_index += 1;
+                return archetype_entities.values[self.current_entity_index - 1]; // entities start at 1
+            } else {
+                self.current_entity_index = 0;
+                self.current_archetype_index += 1;
+                return self.next();
+            }
+        } else {
+            return null;
+        }
+    }
+
+    pub fn count(self: *Self) u64 {
+        var len: u64 = 0;
+        for (self.archetypes.items) |arch| {
+            len += arch.entities.count;
+        }
+        return len;
+    }
+};
+
+fn numMasks(bit_length: usize) usize {
+    return (bit_length + (@bitSizeOf(std.bit_set.DynamicBitSet.MaskInt) - 1)) / @bitSizeOf(std.bit_set.DynamicBitSet.MaskInt);
+}
+
+fn contains(bitset: *const std.bit_set.DynamicBitSet, other: *std.bit_set.DynamicBitSet) bool {
+    const len = @min(numMasks(bitset.unmanaged.bit_length), numMasks(other.unmanaged.bit_length));
+
+    for (bitset.unmanaged.masks[0..len]) |*mask, i| {
+        if (mask.* & other.unmanaged.masks[i] != mask.*) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn intersects(bitset: *const std.bit_set.DynamicBitSet, other: *std.bit_set.DynamicBitSet) bool {
+    const len = @min(numMasks(bitset.unmanaged.bit_length), numMasks(other.unmanaged.bit_length));
+
+    for (bitset.unmanaged.masks[0..len]) |*mask, i| {
+        if (mask.* & other.unmanaged.masks[i] > 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
