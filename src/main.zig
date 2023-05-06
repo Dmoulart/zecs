47eaf8e2971995bcc81d343caa78437e5445a090
Dorian <dorian@resalocal.com>
2023-05-06 12:07:28 +0200

    better bench

--- src/main.zig
+++ src/main.zig
@@ -15,102 +15,131 @@ const Vector = struct { x: f64 = 0, y: f64 = 0 };
 
 // Benchmarks
 pub fn main() !void {
-    {
-        var arena: std.heap.ArenaAllocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-        defer arena.deinit();
+    const thresholds: [5]u32 = [_]u32{ 16_000, 65_000, 262_000, 1_000_000, 2_000_000 };
 
-        const Position = defineComponent(Vector);
-        const Velocity = defineComponent(Vector);
-
-        const n: u32 = 262_000;
-        var world = try World.init(.{ .allocator = arena.child_allocator, .capacity = n });
-        defer world.deinit();
-
-        var i: u32 = 0;
-
-        std.debug.print("\nCreate {} entities with 2 comps", .{n});
-        var before = std.time.milliTimestamp();
-
-        while (i < n) : (i += 1) {
-            var ent = world.createEntity();
-
-            world.attach(ent, &Position);
-            world.attach(ent, &Velocity);
-        }
-
-        var now = std.time.milliTimestamp();
-        std.debug.print("\n{}ms", .{now - before});
-
-        var query = world.query().any(.{Position}).execute();
-        defer query.deinit();
+    for (thresholds[0..thresholds.len]) |n| {
+        try createEntitiesWithTwoComponents(n);
     }
-    {
-        var arena: std.heap.ArenaAllocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-        defer arena.deinit();
 
-        const Position = defineComponent(Vector);
-        const Velocity = defineComponent(Vector);
-
-        const n: u32 = 262_000;
-        var world = try World.init(.{ .allocator = arena.child_allocator, .capacity = n });
-        defer world.deinit();
-
-        var i: u32 = 0;
-
-        std.debug.print("\nRemove and Add a Component in {} entities", .{n});
-
-        while (i < n) : (i += 1) {
-            var ent = world.createEntity();
-
-            world.attach(ent, &Position);
-            world.attach(ent, &Velocity);
-        }
-
-        i = 1;
-        var before = std.time.milliTimestamp();
-        while (i < n) : (i += 1) {
-            world.detach(i, Position);
-            world.attach(i, Position);
-        }
-
-        var now = std.time.milliTimestamp();
-        std.debug.print("\n{}ms", .{now - before});
-
-        var query = world.query().any(.{Position}).execute();
-        defer query.deinit();
+    for (thresholds[0..thresholds.len]) |n| {
+        try removeAndAddAComponent(n);
     }
-    {
-        var arena: std.heap.ArenaAllocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-        defer arena.deinit();
 
-        const Position = defineComponent(Vector);
-        const Velocity = defineComponent(Vector);
-
-        const n: u32 = 100_000;
-        var world = try World.init(.{ .allocator = arena.child_allocator, .capacity = n });
-        defer world.deinit();
-
-        var i: u32 = 0;
-
-        std.debug.print("\nDelete {} Entity", .{n});
-
-        while (i < n) : (i += 1) {
-            var ent = world.createEntity();
-
-            world.attach(ent, &Position);
-            world.attach(ent, &Velocity);
-        }
-
-        i = 1;
-        var before = std.time.milliTimestamp();
-        while (i < n) : (i += 1) {
-            _ = world.deleteEntity(i);
-        }
-
-        var now = std.time.milliTimestamp();
-        std.debug.print("\n{}ms", .{now - before});
-
-        var query = world.query().any(.{Position}).execute();
-        defer query.deinit();
+    for (thresholds[0..thresholds.len]) |n| {
+        try deleteEntities(n);
     }
+}
+
+fn createEntitiesWithTwoComponents(n: u32) !void {
+    var arena: std.heap.ArenaAllocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const Position = defineComponent(Vector);
+    const Velocity = defineComponent(Vector);
+
+    var world = try World.init(.{ .allocator = arena.child_allocator, .capacity = n });
+    defer world.deinit();
+
+    var i: u32 = 0;
+    std.debug.print("\n-------------------------------", .{});
+    std.debug.print("\nCreate {} entities with 2 comps", .{n});
+    std.debug.print("\n-------------------------------", .{});
+    std.debug.print("\n", .{});
+    var before = std.time.milliTimestamp();
+
+    while (i < n) : (i += 1) {
+        var ent = world.createEntity();
+
+        world.attach(ent, &Position);
+        world.attach(ent, &Velocity);
+    }
+
+    var now = std.time.milliTimestamp();
+    std.debug.print("\n", .{});
+    std.debug.print("\nResults : {}ms", .{now - before});
+    std.debug.print("\n", .{});
+    std.debug.print("\n", .{});
+
+    var query = world.query().any(.{Position}).execute();
+    defer query.deinit();
+}
+
+fn removeAndAddAComponent(n: u32) !void {
+    var arena: std.heap.ArenaAllocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const Position = defineComponent(Vector);
+    const Velocity = defineComponent(Vector);
+
+    var world = try World.init(.{ .allocator = arena.child_allocator, .capacity = n });
+    defer world.deinit();
+
+    var i: u32 = 0;
+
+    std.debug.print("\n-------------------------------", .{});
+    std.debug.print("\nRemove and Add a Component in {} entities", .{n});
+    std.debug.print("\n-------------------------------", .{});
+    std.debug.print("\n", .{});
+
+    while (i < n) : (i += 1) {
+        var ent = world.createEntity();
+
+        world.attach(ent, &Position);
+        world.attach(ent, &Velocity);
+    }
+
+    i = 1;
+    var before = std.time.milliTimestamp();
+    while (i < n) : (i += 1) {
+        world.detach(i, Position);
+        world.attach(i, Position);
+    }
+
+    var now = std.time.milliTimestamp();
+    std.debug.print("\n", .{});
+    std.debug.print("\nResults : {}ms", .{now - before});
+    std.debug.print("\n", .{});
+    std.debug.print("\n", .{});
+
+    var query = world.query().any(.{Position}).execute();
+    defer query.deinit();
+}
+
+fn deleteEntities(n: u32) !void {
+    var arena: std.heap.ArenaAllocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const Position = defineComponent(Vector);
+    const Velocity = defineComponent(Vector);
+
+    var world = try World.init(.{ .allocator = arena.child_allocator, .capacity = n });
+    defer world.deinit();
+
+    var i: u32 = 0;
+    std.debug.print("\n-------------------------------", .{});
+    std.debug.print("\nDelete {} Entity", .{n});
+    std.debug.print("\n-------------------------------", .{});
+    std.debug.print("\n", .{});
+
+    while (i < n) : (i += 1) {
+        var ent = world.createEntity();
+
+        world.attach(ent, &Position);
+        world.attach(ent, &Velocity);
+    }
+
+    i = 1;
+    var before = std.time.milliTimestamp();
+    while (i < n) : (i += 1) {
+        _ = world.deleteEntity(i);
+    }
+
+    var now = std.time.milliTimestamp();
+    std.debug.print("\n", .{});
+    std.debug.print("\nResults : {}ms", .{now - before});
+    std.debug.print("\n", .{});
+    std.debug.print("\n", .{});
+
+    var query = world.query().any(.{Position}).execute();
+    defer query.deinit();
 }
